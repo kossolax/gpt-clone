1dee045514f78c08f99345018d8b543c2b2f97ed
Steve Zaretti <kossolax@gmail.com>
2023-05-01 12:37:19 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+from flask_queue_sse import ServerSentEvents
 
 import os
 import subprocess
@@ -10,7 +11,8 @@ import openai
 
 app = Flask(__name__)
 cors = CORS(app, origins=["http://localhost:4200"], expose_headers=["X-Auth-Token"])
-app.config["COMPRESS_REGISTER"] = False
+app.register_blueprint(sse, url_prefix='/stream')
+
 
 @app.route("/")
 def root():
@@ -25,15 +27,18 @@ def chat():
     {"role": "user", "content": "Hello!"}
   ]
   
+  sse = ServerSentEvents()
+  yield sse.response()
+
   responses = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt, temperature=0.9, top_p= 0.1, stream=True)
   for response in responses:
     if len(response.choices) > 0:
       if response.choices[0].delta and response.choices[0].delta.content:
-        json.dumps({
-          "message": response.choices[0].delta.content
-        })
+        sse.send({"message": response.choices[0].delta.content})
       if response.choices[0].finish_reason == "stop":
         break
+  sse.send(event="end")
+  return ""
 
 if __name__ == "__main__":
   # Scaleway's system will inject a PORT environment variable on which your application should start the server.
