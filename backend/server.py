9880042c0bb066ef12a32a43acb08ec385658998
Steve Zaretti <kossolax@gmail.com>
2023-05-01 10:46:22 +0200

    added openai

--- backend/server.py
+++ backend/server.py
@@ -4,12 +4,9 @@ from flask_cors import CORS
 import os
 import subprocess
 import json
-import xmltodict
 import re
 import sys
-
-DEFAULT_PORT = "8080"
-
+import openai
 
 app = Flask(__name__)
 cors = CORS(app, origins=["http://localhost:4200"], expose_headers=["X-Auth-Token"])
@@ -21,9 +18,21 @@ def root():
     "message": "Hello World!"
   })
 
+@app.route("/chat")
+def root():
+  prompt = [
+    {"role": "system", "content": "You are a helpfull assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+  
+  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt, temperature=0.9, top_p= 0.1)
+  return json.dumps({
+    "message": completion.choices[0].message.content
+  })
 
 if __name__ == "__main__":
   # Scaleway's system will inject a PORT environment variable on which your application should start the server.
-  port_env =  os.getenv("PORT", DEFAULT_PORT)
+  port_env =  os.getenv("PORT", "8080")
+  openai.api_key = os.getenv("OPENAI_KEY")
   port = int(port_env)
   app.run(debug=False, host="0.0.0.0", port=port)
