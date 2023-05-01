a08341d1cac31578a72906e160d5de10aa065e09
Steve Zaretti <kossolax@gmail.com>
2023-05-01 16:42:58 +0200

    Update chat.ts

--- src/app/classes/chat.ts
+++ src/app/classes/chat.ts
@@ -1,10 +1,12 @@
-type ChatInput =
-  | {
-      role: "system" | "user";
-      content: string;
-    }
-  | {
-      role: "assistant";
-      content: string;
-      writing: boolean;
-    };
+export interface SystemUserInput {
+  role: "system" | "user";
+  content: string;
+}
+
+export interface AssistantInput {
+  role: "assistant";
+  content: string;
+  writing: boolean;
+}
+
+export type ChatInput = SystemUserInput | AssistantInput;
